9d01b09354868a76b0f2dccd2f032c8a1ecccbe0
SandeeepS <sandeepsanthosh3303@gmail.com>
2023-08-28 14:59:06 +0530

    eigth commit

--- script.js
+++ script.js
@@ -2,10 +2,13 @@ const express = require('express');
 const session = require('express-session');
 const app = express();
 const bodyParser = require('body-parser');
+const flash = require('connect-flash');
 app.set('view engine','ejs');
 app.use(express.static("views"));
 app.use(express.static(__dirname));
 app.use(express.urlencoded());
+app.use(flash());
+
 
 //mongodb connection
 const mongoose = require('mongoose');
@@ -29,7 +32,9 @@ const sch = {
   email:String,
   pass:Number
 }
+
 const monmodel = mongoose.model("users",sch);
+const monmodelA = mongoose.model("admins",sch);
 
 
 
@@ -49,9 +54,6 @@ app.use(session({
   cookie:{maxAge:oneDay}
 }));
 
-//accessing body parser
-const use = "sandeep";
-const pass = "123"
 
 
 app.get('/',isAuthenticated,(req,res)=>{
@@ -80,19 +82,42 @@ function isAuthenticated(req, res, next) {
   }
 }
 
+// Middleware to check if the admin is authenticated
+function isAuthenticatedAdmin(req, res, next) {
+  if ( req.session.user2) {
+    res.redirect('/admin'); // admin is authenticated, proceed to the next route handler
+  } else {
+    return next(); // admin is not authenticated, redirect to adminlogin page
+  }
+}
+
 
 //login
-app.post('/login',(req,res)=>{
- console.log(req.body)
-  const{username,password}= req.body;
-  if(username === use && password === pass){
-    req.session.user=username;//store the user name in the session
-    res.redirect('/index')
-  }
-  else{
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+  console.log(username);
+  console.log(password);
+
+  try {
+    // Query the database to find a user with the given username and password
+    
+    const user = await monmodel.findOne({ name: username, pass: password }).exec();
+
+    if (user) {
+      // User exists in the database, set the session and redirect to index page
+      req.session.user = user.name; // Store the user's name in the session
+      res.redirect('/index');
+    } else {
+      // User does not exist or the password is incorrect, redirect to login page with a flash message
+      req.flash('error', 'Invalid username or password. Please sign up if you are a new user.');
+      res.redirect('/');
+    }
+  } catch (error) {
+    console.error("Error during login:", error);
     res.redirect('/');
   }
-})
+});
+
 
 
 //signup
@@ -136,6 +161,86 @@ app.get('/logout',(req,res)=>{
 });
 
 
+
+//Admin
+app.get('/adminlogin',(req,res)=>{
+  if(!req.session.user2){
+    res.render('adminlogin');
+  }else{
+    res.redirect("/admin")
+  }
+ 
+});
+
+
+app.get('/admin',async (req, res) => {
+
+  if(!req.session.user2){
+    res.redirect('/adminlogin');
+  }else{
+    try {
+    
+      // Query the database to get all user records
+      const users = await monmodel.find().exec();
+      
+      // Query the database to get all admin records
+      const admins = await monmodelA.find().exec();
+  
+      res.render('admin', { users, admins });
+    } catch (error) {
+      console.error("Error fetching user and admin details:", error);
+      res.redirect('/adminlogin');
+    }
+
+  }
+
+
+  
+});
+
+
+app.post('/adlogin', async (req, res) => {
+  const { adUsername, adPassword } = req.body;
+  console.log(adUsername);
+  console.log(adPassword);
+
+  try {
+    // Query the database to find a user with the given username and password
+    
+    const user2 = await monmodelA.findOne({ name: adUsername, pass: adPassword }).exec();
+    console.log("User found in the database:", user2);
+
+    if (user2) {
+      // User exists in the database, set the session 
+      req.session.user2 = user2.name;// Store the user's name in the session
+      console.log(req.session.user2);
+      res.redirect('/admin');
+    } else {
+      // User does not exist or the password is incorrect, redirect to login page with a flash message
+      req.flash('error', 'Invalid username or password. Please sign up if you are a new user.');
+      res.redirect('/adminlogin');
+    }
+  } catch (error) {
+    console.error("Error during login:", error);
+    res.redirect('/adminlogin');
+  }
+});
+
+
+//logout admin
+app.get('/logoutAd',(req,res)=>{
+  req.session.destroy((err)=>{
+    if(err){
+      console.error('Error destroyng session',err);
+    }else{
+     
+      res.redirect('/');
+    
+    }
+   
+  });
+ 
+});
 
 app.listen(5000, () => {
   console.log('Server running on http://localhost:5000');
